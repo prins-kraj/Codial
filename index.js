0d8b762da4053434fee3f9a3187d1143e63f2b90
Prince Kumar <princesansaraj@gmail.com>
2023-03-03 11:20:56 +0530

    index added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
