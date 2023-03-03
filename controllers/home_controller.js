d350173ee528c8e142155dbe6969a2795155facf
Prince Kumar <princesansaraj@gmail.com>
2023-03-03 20:50:22 +0530

    views and ejs added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial!</h1>')
+    // return res.end('<h1>Express is up for Codeial!</h1>');
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
 // module.exports.actionName = function(req, res){}
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
