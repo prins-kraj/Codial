695a9e916051c69d3685f313d0db7f73b2cacd7c
Prince Kumar <princesansaraj@gmail.com>
2023-03-04 15:34:51 +0530

    cookie added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial!</h1>');
+
+    res.cookie('User_id', 25);
+    console.log(req.cookies);
     return res.render('home', {
         title: "Home"
     });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 
@@ -6,7 +7,12 @@ const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 app.use(express.static('./assets'));
+
 app.use(expressLayouts);
 
 // extract style and scripts from sub pages into the layout
