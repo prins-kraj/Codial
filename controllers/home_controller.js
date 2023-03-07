16e97f887e1b483e2f85acb242ded2b527757920
Prince Kumar <princesansaraj@gmail.com>
2023-03-07 17:10:36 +0530

    user name added on post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,23 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial!</h1>');
 
-    res.cookie('User_id', 25);
-    console.log(req.cookies);
-    return res.render('home', {
-        title: "Home"
+    // res.cookie('User_id', 25);
+    // console.log(req.cookies);
+    // Post.find({}, function (err, posts) {
+    //     return res.render('home', {
+    //         title: "Codial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post 
+    Post.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts
+        });
     });
 }
 
