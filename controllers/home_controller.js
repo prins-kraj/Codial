cc6a74ba3c7f979225f073d5e2895cecaceb03ad
Prince Kumar <princesansaraj@gmail.com>
2023-03-08 01:33:59 +0530

    comment shown to post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,15 @@ module.exports.home = function(req, res){
     // });
 
     // populate the user of each post 
-    Post.find({}).populate('user').exec(function (err, posts) {
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function (err, posts) {
         return res.render('home', {
             title: "Codial | Home",
             posts: posts
--- models/post.js
+++ models/post.js
@@ -13,7 +13,7 @@ const postSchema = new mongoose.Schema({
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'comment'
+            ref: 'Comment'
         }
     ]
 },{
