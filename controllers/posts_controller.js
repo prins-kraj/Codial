af265fd383d846fd249d059966134b86b82c13f2
Prince Kumar <princesansaraj@gmail.com>
2023-03-08 02:30:43 +0530

    Deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -8,5 +9,21 @@ module.exports.create = function (req, res) {
         if (err) {console.log('error in creating a post'); return;}
 
         return res.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function (err) {
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
