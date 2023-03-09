756ee3b0ecbd1320e17737ab88d87f2f13f19d32
Prince Kumar <princesansaraj@gmail.com>
2023-03-09 09:36:35 +0530

    comment authorisation done'

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create',passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 
 module.exports=router;
