01d51d29f2a034b99c75b23be87b174c19045555
Prince Kumar <princesansaraj@gmail.com>
2023-03-11 01:22:02 +0530

    comments cnverted in ajax

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -10,13 +10,30 @@ module.exports.create = async function (req, res) {
                 post: req.body.post,
                 user: req.user._id
             });
+            
             post.comments.push(comment);
             post.save();
+
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
 
             res.redirect('back');
         }
     } catch (err) {
-        console.log('Error', err);
+        // console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
@@ -31,13 +48,28 @@ module.exports.destroy = async function (req, res) {
             comment.remove();
 
             let post = await Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});
+            
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
             return res.redirect('back');
             
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('Error', err);
+        // console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
