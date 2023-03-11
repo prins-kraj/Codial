2988cc2ca686a070facf5e097fd09034dd15200c
Prince Kumar <princesansaraj@gmail.com>
2023-03-12 02:38:07 +0530

    api post deletion done

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,53 @@
-module.exports.index = function (req, res) {
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment')
+
+module.exports.index = async function (req, res) {
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.json(200, {
         message: "List of posts",
-        posts: []
+        posts: posts
     })
+}
+
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // if (post.user == req.user.id) {
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+            // if (req.xhr) {
+            //     return res.status(200).json({
+            //         data: {
+            //             post_id: req.params.id
+            //         },
+            //         message: 'Post deleted'
+            //     });
+            // }
+
+            // req.flash('success', 'Post deleted!');
+            return res.json(200, {
+                message: 'Post and related comment deleted successfully'
+            })
+        // }else{
+        //     req.flash('error', ' You cannot delete post!');
+        //     return res.redirect('back');
+        // }
+    } catch (err) {
+        return res.json(500, {
+            message: 'Internal server error'
+        })
+    }
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,5 +5,7 @@ const postsApi = require('../../../controllers/api/v1/posts_api');
 
 router.get('/', postsApi.index);
 
+router.delete('/:id', postsApi.destroy);
+
 
 module.exports = router
