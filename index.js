61135eb5cc540d4ae3c7d323b456d783b3bbbe19
Prince Kumar <princesansaraj@gmail.com>
2023-03-24 03:32:50 +0530

    chat engine created

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// setup the chat server to be used wuth socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port: 5000');
+
+
+
 // node-sass-middlewaare unable to install
 // const sassMiddleware =require('node-sass-middleware');
 
